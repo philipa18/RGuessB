147777efb10d7133f8fd63fc3d38b875423cf5d1
Philip Alexander <philipalexander39@gmail.com>
2020-06-29 23:56:22 +0100

    Removed unnecessary calculation and documented Game Activity.

--- app/src/main/java/com/example/rguessb/GameActivity.java
+++ app/src/main/java/com/example/rguessb/GameActivity.java
@@ -24,14 +24,17 @@ public class GameActivity extends AppCompatActivity {
     int blueGuess;
 
     @Override
+    // Sets up the game itself
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        // Displays randomly-generated color.
         findViewById(R.id.viewColor).setBackgroundColor(getRandomColor());
+
         sbRed = findViewById(R.id.seekBarRed);
         sbGreen = findViewById(R.id.seekBarGreen);
         sbBlue = findViewById(R.id.seekBarBlue);
-        //Changing the displayed value of the color when user moves seek bar
+        // Changes the displayed value of the users guess for each color when they move the dials.
         sbRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -84,20 +87,32 @@ public class GameActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Generates the random color to be displayed color to be displayed.
     public int getRandomColor(){
+        // Random number generator that will be used for each basic color.
         Random rnd = new Random();
+        /* Intensity of red, green, and blue are each designated in the RGB code by an integer in
+           the range[0,255], so a bound of 256 will be used for the RNG. */
         redVal = rnd.nextInt(256);
         blueVal = rnd.nextInt(256);
         greenVal = rnd.nextInt(256);
+        // Color object with composite color is returned.
+        // Alpha value of 255 is used as we want the color to be completely opaque.
         return Color.argb(255, redVal, blueVal, greenVal);
     }
 
+    // Will calculate score and send it to the final screen to be displayed.
     public void endRound(View view){
+        // Intent that will carry score to Score Activity
         Intent intent = new Intent(this, ScoreActivity.class);
+        // Difference between user guess and actual value of each color is found.
         int redDif = Math.abs(redVal - redGuess);
         int blueDif = Math.abs(blueVal - blueGuess);
         int greenDif = Math.abs(greenVal - greenGuess);
-        int score = 255*3 - redDif - blueDif - greenDif;
+        // Differences are all subtracted from 765, the maximum cumulative difference.
+        int score = 765 - redDif - blueDif - greenDif;
+        // Text for score display is placed on Score Activity before activity itself is displayed.
         intent.putExtra("com.example.rguessb.score", "Score: "+ String.valueOf(score));
         startActivity(intent);
 
